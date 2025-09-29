adf31b08bfeeb0ebe3877f686b80b20feb710999
Leo Huckvale <leohuckvale@gmail.com>
2025-09-29 10:01:53 +0100

    Change call signatures in example

--- example.py
+++ example.py
@@ -3,20 +3,22 @@ Script to load example data and execute optimisation model
 """
 from pathlib import Path
 
-from chronos.lite import BatteryConfig, MarketData, BatteryModel
+from chronos.lite.data import load_battery_config, load_market_data
+from chronos.lite.model import Model
 
 DATA_DIR = Path(__file__).parent / "data"
 
 
 def main():
-    battery_config = BatteryConfig(DATA_DIR / "battery_parameters.csv")
-    market_data = MarketData(
+    battery_config = load_battery_config(DATA_DIR / "battery_parameters.csv")
+    market_data = load_market_data(
         half_hourly_csv=DATA_DIR / "half-hourly-market.csv",
         hourly_csv=DATA_DIR / "hourly-market.csv",
     )
-    model = BatteryModel(battery_config, market_data)
+    model = Model(battery_config, market_data)
     model.optimise()
     print(model.solution)
+
 
 if __name__ == "__main__":
     main()
